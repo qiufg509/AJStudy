d4c3e67de804cbd158b20c9e62e79b4cd090c736
qiufengguang <qfguang@163.com>
2025-12-25 00:21:52 +0800

    TicketNo: Description: 处理切换主题时底部半透明导航栏闪烁的问题、去掉底部导航栏高程阴影（分割线） Feature or Bugfix: Bugfix Binary Source: No

--- app/src/main/java/com/qiufengguang/ajstudy/activity/main/MainActivity.java
+++ app/src/main/java/com/qiufengguang/ajstudy/activity/main/MainActivity.java
@@ -7,6 +7,7 @@ import android.text.TextUtils;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDestination;
@@ -37,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
     private MainViewModel viewModel;
 
     private GlobalViewModel globalVm;
+
+    private NavController navController;
 
     @Override
     protected void onNewIntent(@NonNull Intent intent) {
@@ -87,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
         if (navHostFragment == null) {
             return;
         }
-        NavController navController = navHostFragment.getNavController();
+        navController = navHostFragment.getNavController();
 
         // 设置导航图
         NavGraph navGraph = navController.getNavInflater().inflate(R.navigation.main_navigation);
@@ -100,25 +103,44 @@ public class MainActivity extends AppCompatActivity {
 
         binding.navView.setOnItemSelectedListener(item -> {
             int itemId = item.getItemId();
-            NavDestination destination = navController.getCurrentDestination();
-            if (destination != null && destination.getId() == itemId) {
-                // BottomNavigationView会保持原item的选中状态，如果返回false它会先取消选中，然后重新选中，可能闪烁
-                return true;
-            }
-            if (itemId == R.id.navigation_me) {
-                if (globalVm == null || !globalVm.isLoggedIn()) {
-                    Intent intent = new Intent(this, LoginActivity.class);
-                    intent.putExtra(LoginAction.ORIGINAL_PAGE, MainActivity.class.getName());
-                    intent.putExtra(LoginAction.DESTINATION_ID, itemId);
-                    startActivity(intent);
-                    // "监听器没有处理这次点击",BottomNavigationView 会尝试恢复之前的选中状态
-                    return false;
-                }
-            }
-            navController.navigate(itemId);
-            viewModel.setLiveData(itemId);
-            return true;
+            return onNavItemSelected(itemId);
         });
+    }
+
+    /**
+     * 已经选中导航栏后的处理（点中的item以及是选中状态）
+     *
+     * @param itemId menu中定义的item id
+     * @return true已经处理导航操作，BottomNavigationView会保持原item的选中状态
+     * false已经选中的item先取消选中，然后重新选中上一次选中的item，可能闪烁，拦截并跳转页面时使用
+     */
+    private boolean onNavItemSelected(int itemId) {
+        NavDestination destination = navController.getCurrentDestination();
+        if (destination != null && destination.getId() == itemId) {
+            // BottomNavigationView会保持原item的选中状态，如果返回false它会先取消选中，然后重新选中，可能闪烁
+            return true;
+        }
+        if (itemId == R.id.navigation_me) {
+            if (globalVm == null || !globalVm.isLoggedIn()) {
+                Intent intent = new Intent(this, LoginActivity.class);
+                intent.putExtra(LoginAction.ORIGINAL_PAGE, MainActivity.class.getName());
+                intent.putExtra(LoginAction.DESTINATION_ID, itemId);
+                startActivity(intent);
+                // "监听器没有处理这次点击",BottomNavigationView 会尝试恢复之前的选中状态
+                return false;
+            }
+        }
+        if (itemId == R.id.navigation_know_how) {
+            // 使用根布局设置颜色的页面设置透明渐变背景
+            binding.navView.setBackgroundResource(R.drawable.bottom_nav_background_blur);
+        } else {
+            // 子页面使用backgroundImage的页面设置不透明背景（以及让出导航栏空间，避免闪烁）
+            binding.navView.setBackgroundColor(
+                ContextCompat.getColor(this, R.color.ajstudy_window_background));
+        }
+        navController.navigate(itemId);
+        viewModel.setLiveData(itemId);
+        return true;
     }
 
     /**
